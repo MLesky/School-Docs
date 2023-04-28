2dbf6174415191f8a2df31711433c63f52fa15fe
Randy Ankiambom <jamrandyankiambom@gmail.com>
2023-04-28 16:51:39 +0100

    add raw data

---
+++ lib/app/reading/models/document.dart
@@ -0,0 +1,8 @@
+
+class Document {
+  final String id;
+  final String title;
+  final String lecturersName;
+  final String coverImagePath;
+  Document({this.id = '', required this.title, this.lecturersName = '', this.coverImagePath = ''});
+}
